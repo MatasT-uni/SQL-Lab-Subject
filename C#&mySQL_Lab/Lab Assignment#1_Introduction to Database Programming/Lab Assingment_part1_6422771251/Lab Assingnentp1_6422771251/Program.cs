019def8adf707e8c8377105eeef6c510b4ce6f88
Matas Thanamee <144789263+MatasT-uni@users.noreply.github.com>
2024-01-15 23:05:14 +0700

    Add files via upload

---
+++ C#&mySQL_Lab/Lab Assignment#1_Introduction to Database Programming/Lab Assingment_part1_6422771251/Lab Assingnentp1_6422771251/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Problem1
+{
+    class p1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number:");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter the second number:");
+            string num2 = Console.ReadLine();
+
+            //convert string to int
+            int numtocal1 = Convert.ToInt32(num1);
+            int numtocal2 = Convert.ToInt32(num2);
+
+            //for check
+            //Console.WriteLine("First is: " + num1);
+            //Console.WriteLine("Second is: " + num2);
+
+            //Console.WriteLine("First is: " + numtocal1);
+            //Console.WriteLine("Second is: " + numtocal2);
+
+            //Calculates
+            int result1sum = numtocal1+ numtocal2;
+            int result2difference = numtocal1- numtocal2;
+            int result3product = numtocal1 * numtocal2;
+            int result4quotient = numtocal1/ numtocal2;
+
+            Console.WriteLine("Sum is: " + result1sum);
+            Console.WriteLine("Difference is: " + result2difference);
+            Console.WriteLine("Product is: " + result3product);
+            Console.WriteLine("Quotient is: " + result4quotient);
+        }
+    }
+}
---
+++ C#&mySQL_Lab/Lab Assignment#1_Introduction to Database Programming/Lab Assingment_part2_6422771251/LabAssingmentp2_6422771251/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+namespace Problem2
+{
+    class p2
+    {
+        static void Main(string[] args)
+        {
+
+            int N1;
+            Console.Write("Enter N1: ");
+            N1 = int.Parse(Console.ReadLine());
+            int N2;
+            Console.Write("Enter N2: ");
+            N2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while( N1 != 0 && N2 != 0)
+            {
+                for (int i = N1; i <= N2; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine("The sum of the sequence number from {0} to {1} = {2}", N1, N2, sum);
+                Console.ReadLine();
+                break;
+            }
+                   
+        }
+    }
+}
